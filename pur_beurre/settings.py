3fde8d8b8accc84576e96473488b1b46bfa9d068
pierro145 <pierremunier@hotmail.fr>
2020-09-06 15:36:56 +0200

    update

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
-import environ
 
-env = environ.Env()
-environ.Env.read_env()
+
+env = Env()
+Env.read_env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
