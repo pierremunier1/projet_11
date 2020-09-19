e8e05196a5fa81c653a9e7a3a15ae1ae22800b92
pierro145 <pierremunier@hotmail.fr>
2020-09-19 16:53:42 +0200

    update

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', SignUpView.as_view(), name='signup'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
 ]
