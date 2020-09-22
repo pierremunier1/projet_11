09e78507fb2eadccd8647efda573810889301f40
Thierry Chappuis <tchappui@gmail.com>
2020-09-22 15:16:20 +0200

    Refactored exports.helpers for a better serialization of the data.

--- exports/views.py
+++ exports/views.py
@@ -1,11 +1,11 @@
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 
-from .helpers import get_exports
+from .helpers import Exporter
 
 
 @login_required
 def export(request):
     """Exports instances as a json file."""
-    collection = get_exports()
-    return JsonResponse(collection, safe=False)
+    exporter = Exporter()
+    return JsonResponse(exporter.data, safe=False)
